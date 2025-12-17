2f8da6bc59b357b7e65c2ad4f032d7e28b3ff9a5
IvanMiyasoied <vokar009@gmail.com>
2025-12-17 14:19:21 +0200

    5 ex commited

---
+++ unit_05.js
@@ -0,0 +1,115 @@
+// const button1 = document.getElementById('b-1');
+// const out1 = document.getElementById('out-1');
+
+
+// button1.addEventListener("click",() => {
+
+//     out1.innerHTML = '';
+    
+
+//     for (let i = 1; i <= 50; i++) {
+//         setTimeout(() => {
+//             out1.textContent += i + ' '
+
+//         },i * 200)
+       
+//     }
+
+
+// });
+
+
+
+
+// const button2 = document.getElementById('b-2');
+// const out2 = document.getElementById('out-2');
+
+
+// button2.addEventListener("click",() => {
+
+//     out2.innerHTML = '';
+    
+
+//     for (let i = 2; i <= 122; i+=2) {
+//         setTimeout(() => {
+//             out2.textContent += i + ' ';
+
+//         },i * 100)
+       
+//     }
+
+
+// });
+
+
+
+
+// const button3 = document.getElementById('b-3');
+// const out3 = document.getElementById('out-3');
+
+
+// button3.addEventListener("click",() => {
+
+//     out3.innerHTML = '';
+    
+//     for (let i = 25; i >= 7; i--) {
+//         setTimeout(() => {
+//             out3.textContent += i + ' ';
+
+//         },(25-i) * 100)
+       
+//     }
+    
+
+// });
+
+
+// const button4 = document.getElementById('b-4');
+// const out4 = document.getElementById('out-4');
+
+
+// button4.addEventListener("click",() => {
+
+//     out4.innerHTML = '';
+    
+//     for (let i = 77; i >= 35; i-=3) {
+//         setTimeout(() => {
+//             out4.textContent += i + '_';
+
+//         },(77-i) * 100)
+       
+//     }
+    
+
+// });
+
+
+// const button5 = document.getElementById('b-5');
+// const out5 = document.getElementById('out-5');
+
+
+// button5.addEventListener("click",() => {
+
+//     out5.innerHTML = '';
+    
+//     for (let i = 1; i <= 17; i++) {
+//         setTimeout(() => {
+//             if (i % 2 === 0) {
+//                 out5.textContent += i + '_**';
+
+//             } else if (i % 2 !== 0) {
+//                 out5.textContent += i + '_*';
+//             }
+
+//         },i * 200)
+       
+//     }
+    
+
+// });
+
+
+const input6 = document.getElementById('input6');
+const button6 = document.getElementById('b-6');
+const out6 = document.getElementById('out-6');
+
